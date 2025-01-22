a287322e9949e2e3bd6504f2cd8616fa7d4decb9
Wellik Perroni <welliksp@gmail.com>
2025-01-22 14:56:13 -0300

    initial commit

---
+++ src/test/java/br/com/wsp/transfer_api/TransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wsp.transfer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
